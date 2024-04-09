c042e9905f8441a36d7974a02ad28877f81b4bd4
Srividhya Varadhan <100423362+Srividhya0504@users.noreply.github.com>
2024-04-09 19:01:38 +0530

    Update User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -11,6 +11,7 @@ import javax.crypto.SecretKey;
 
 public class User {
   public String id, username, hashedPassword;
+   public String id, username, hashedPassword;
 
   public User(String id, String username, String hashedPassword) {
     this.id = id;
